20a8b0e9068942abc2cc3323a176664bd017a045
Hidayet <hdytocln88@gmail.com>
2025-11-04 13:11:21 +0300

    dosyalar oluşturuldu

--- KitapTakipSistemi/models/Kitap.cs
+++ KitapTakipSistemi/models/Kitap.cs
@@ -1,1 +1,27 @@
-﻿
+﻿namespace KitapTakipSistemi.Models
+{
+    public class Kitap
+    {
+      
+        public int Id { get; private set; } 
+        public string Ad { get; set; }
+        public string Yazar { get; set; }
+        public bool Durum { get; set; } 
+
+        public Kitap(int id, string ad, string yazar)
+        {
+            
+            this.Id = id;
+            this.Ad = ad;
+            this.Yazar = yazar;
+            this.Durum = true; 
+        }
+
+        
+        public override string ToString()
+        {
+            string durumStr = this.Durum ? "Müsait" : "Ödünçte";
+            return $"ID: {this.Id}, Ad: {this.Ad}, Yazar: {this.Yazar}, Durum: {durumStr}";
+        }
+    }
+}
