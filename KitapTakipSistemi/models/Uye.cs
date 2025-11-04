864190774b7b78931d0ff43855ab0619b42f3864
Hidayet <hdytocln88@gmail.com>
2025-11-04 23:05:32 +0300

    üyenin id,ad soyad ve ödünç aldığı kitap bilgisini tutar

--- KitapTakipSistemi/models/Uye.cs
+++ KitapTakipSistemi/models/Uye.cs
@@ -1,1 +1,25 @@
 ﻿
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KitapTakipSistemi.Models
+{
+    public class Uye
+    {
+        
+        public int Id { get; private set; }
+        public string AdSoyad { get; set; }
+
+       
+        public List<Kitap> OduncAlinanKitaplar { get; private set; }
+
+        public Uye(int id, string adSoyad)
+        {
+            this.Id = id;
+            this.AdSoyad = adSoyad;
+            this.OduncAlinanKitaplar = new List<Kitap>();
+        }
+
+    }
+}
