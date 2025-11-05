d457872c5e05fbb90c3def7914ef98dc65874b9a
Hidayet <hdytocln88@gmail.com>
2025-11-05 13:24:02 +0300

    bu metot, nesnenin bilgilerini (ID, Ad Soyad ve ödünç alınan kitapların adlarını) tek bir metin olarak düzenleyip döndürür; eğer hiç ödünç kitap yoksa "yok" yazacak.

--- KitapTakipSistemi/models/Kutuphane.cs
+++ KitapTakipSistemi/models/Kutuphane.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KitapTakipSistemi.Models;
+
+namespace KitapTakipSistemi
+{
+    public class Kutuphane
+    {
+        public List<Kitap> Kitaplar { get; private set; }
+        public List<Uye> Uyeler { get; private set; }
+
+        private int nextKitapId = 1;
+        private int nextUyeId = 1;
+
+        public Kutuphane()
+        {
+            this.Kitaplar = new List<Kitap>();
+            this.Uyeler = new List<Uye>();
+        }
+
+        // metotlar
+
+        public void KitapEkle(string ad, string yazar)
+        {
+            try
+            {
+                var yeniKitap = new Kitap(nextKitapId++, ad, yazar);
+                this.Kitaplar.Add(yeniKitap);
+                Console.WriteLine($"\n Kitap Başarıyla Eklendi: {yeniKitap}");
+            }
+            catch (Exception ex)
+            {
+                // Zorunlu Özellik: Try-Catch Kullanımı
+                Console.WriteLine($"\n Kitap eklenirken bir hata oluştu: {ex.Message}");
+            }
+        }
+    }
+}
+
--- KitapTakipSistemi/models/Uye.cs
+++ KitapTakipSistemi/models/Uye.cs
@@ -7,11 +7,11 @@ namespace KitapTakipSistemi.Models
 {
     public class Uye
     {
-        
+
         public int Id { get; private set; }
         public string AdSoyad { get; set; }
 
-       
+
         public List<Kitap> OduncAlinanKitaplar { get; private set; }
 
         public Uye(int id, string adSoyad)
@@ -20,6 +20,15 @@ namespace KitapTakipSistemi.Models
             this.AdSoyad = adSoyad;
             this.OduncAlinanKitaplar = new List<Kitap>();
         }
+        public override string ToString()
+        {
 
+            string kitaplar = string.Join(",", OduncAlinanKitaplar.Select(k => k.Ad));//virgülle ayırdık ödünç alınan kitapları.
+            if (string.IsNullOrWhiteSpace(kitaplar))
+                kitaplar = "yok";
+
+            return $"ID: {this.Id}, Ad Soyad: {this.AdSoyad}, Ödünçteki Kitaplar: {kitaplar}";
+
+        }
     }
 }
